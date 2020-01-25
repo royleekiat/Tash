040705aa4c347a12cf52995b7af1295a636c9370
roy.lee <roy.lee@cirruspoc.onmicrosoft.com>
2020-01-26 00:41:06 +0800

    filter by benchmark

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var path = require('path');
 var schema = buildSchema(`
     type Query {
         datapoint(id: Int!): Datapoint
-        datapoints(portfolio: String): [Datapoint]
+        datapoints(portfolio: String, benchmark: String): [Datapoint]
     },
     type Datapoint {
         id: Int
@@ -163,6 +163,152 @@ var datapointsList = [
         period: '2018-06-01',
         value: 155231,
         portfolio: '40% VTSMX (Stock) + 60% VBMFX (Bond)',
+    },
+    {
+        id: 25,
+        period: '2017-07-01',
+        value: 115235,
+        portfolio: '20% VTSMX (Stock) + 80% VBMFX (Bond)',
+    },
+    {
+        id: 26,
+        period: '2017-08-01',
+        value: 135235,
+        portfolio: '20% VTSMX (Stock) + 80% VBMFX (Bond)',
+    },
+    {
+        id: 27,
+        period: '2017-09-01',
+        value: 125235,
+        portfolio: '20% VTSMX (Stock) + 80% VBMFX (Bond)',
+    },
+    {
+        id: 28,
+        period: '2017-10-01',
+        value: 115231,
+        portfolio: '20% VTSMX (Stock) + 80% VBMFX (Bond)',
+    },
+    {
+        id: 29,
+        period: '2017-11-01',
+        value: 135235,
+        portfolio: '20% VTSMX (Stock) + 80% VBMFX (Bond)',
+    },
+    {
+        id: 30,
+        period: '2017-12-01',
+        value: 145235,
+        portfolio: '20% VTSMX (Stock) + 80% VBMFX (Bond)',
+    },
+    {
+        id: 31,
+        period: '2018-01-01',
+        value: 141235,
+        portfolio: '20% VTSMX (Stock) + 80% VBMFX (Bond)',
+    },
+    {
+        id: 32,
+        period: '2018-02-01',
+        value: 135231,
+        portfolio: '20% VTSMX (Stock) + 80% VBMFX (Bond)',
+    },
+    {
+
+        id: 33,
+        period: '2018-03-01',
+        value: 125235,
+        portfolio: '20% VTSMX (Stock) + 80% VBMFX (Bond)',
+    },
+    {
+        id: 34,
+        period: '2018-04-01',
+        value: 115235,
+        portfolio: '20% VTSMX (Stock) + 80% VBMFX (Bond)',
+    },
+    {
+        id: 35,
+        period: '2018-05-01',
+        value: 105235,
+        portfolio: '20% VTSMX (Stock) + 80% VBMFX (Bond)',
+    },
+    {
+        id: 36,
+        period: '2018-06-01',
+        value: 105231,
+        portfolio: '20% VTSMX (Stock) + 80% VBMFX (Bond)',
+    },
+    {
+        id: 37,
+        period: '2017-07-01',
+        value: 145235,
+        portfolio: '60% VTSMX (Stock) + 40% VBMFX (Bond)',
+    },
+    {
+        id: 38,
+        period: '2017-08-01',
+        value: 142235,
+        portfolio: '60% VTSMX (Stock) + 40% VBMFX (Bond)',
+    },
+    {
+        id: 39,
+        period: '2017-09-01',
+        value: 135235,
+        portfolio: '60% VTSMX (Stock) + 40% VBMFX (Bond)',
+    },
+    {
+        id: 40,
+        period: '2017-10-01',
+        value: 145231,
+        portfolio: '60% VTSMX (Stock) + 40% VBMFX (Bond)',
+    },
+    {
+        id: 41,
+        period: '2017-11-01',
+        value: 155235,
+        portfolio: '60% VTSMX (Stock) + 40% VBMFX (Bond)',
+    },
+    {
+        id: 42,
+        period: '2017-12-01',
+        value: 163235,
+        portfolio: '60% VTSMX (Stock) + 40% VBMFX (Bond)',
+    },
+    {
+        id: 43,
+        period: '2018-01-01',
+        value: 161235,
+        portfolio: '60% VTSMX (Stock) + 40% VBMFX (Bond)',
+    },
+    {
+        id: 44,
+        period: '2018-02-01',
+        value: 165231,
+        portfolio: '60% VTSMX (Stock) + 40% VBMFX (Bond)',
+    },
+    {
+
+        id: 45,
+        period: '2018-03-01',
+        value: 175235,
+        portfolio: '60% VTSMX (Stock) + 40% VBMFX (Bond)',
+    },
+    {
+        id: 46,
+        period: '2018-04-01',
+        value: 160235,
+        portfolio: '60% VTSMX (Stock) + 40% VBMFX (Bond)',
+    },
+    {
+        id: 47,
+        period: '2018-05-01',
+        value: 150235,
+        portfolio: '60% VTSMX (Stock) + 40% VBMFX (Bond)',
+    },
+    {
+        id: 48,
+        period: '2018-06-01',
+        value: 155231,
+        portfolio: '60% VTSMX (Stock) + 40% VBMFX (Bond)',
     }
 ]
 var getDatapoint = function(args) { 
@@ -174,7 +320,8 @@ var getDatapoint = function(args) {
 var getDatapoints = function(args) {
     if (args.portfolio) {
         var portfolio = args.portfolio;
-        return datapointsList.filter(datapoint => datapoint.portfolio === portfolio);
+        var benchmark = args.benchmark;
+        return datapointsList.filter(datapoint => datapoint.portfolio === portfolio || datapoint.portfolio === benchmark);
     } else {
         return datapointsList;
     }
